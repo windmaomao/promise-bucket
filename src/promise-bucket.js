a8727019553e26cfeb6e7857d4e3c868a90572a0
Fang Jin <windmaomao@gmail.com>
2015-12-27 17:03:06 -0500

    add promise resolve and reject

--- src/promise-bucket.js
+++ src/promise-bucket.js
@@ -2,16 +2,37 @@
 
 angular
     .module('fangAngular', [])
-    .service('PromiseBucket', function($q, $log) {
+    .service('PromiseBucket', function($log, $q) {
         // log
         this.log = function(name) {
             $log.info(name);
         };
+        // resolve
+        this.resolve = function(res) {
+            return res;
+        };
+        // reject
+        this.reject = function(res) {
+            return res;
+        };
         // call the promise
         this.hit = function(name, promise) {
             this.log(name);
+            var that = this;
+            // if promise is specified
             if (promise) {
-                return promise();
+                // if promise is a promise
+                if (angular.isFunction(promise.then)) {
+                    var deferred = $q.defer();
+                    promise().then(function(res) {
+                        deferred.resolve(that.resolve(res));
+                    }, function(res) {
+                        deferred.reject(that.reject(res));
+                    });
+                    return deferred.promise;
+                } else {
+                    return $q.when(promise());
+                }
             } else {
                 throw new Error('Promise is not valid');
             }
--- tests/specs/promiseBucket.spec.js
+++ tests/specs/promiseBucket.spec.js
@@ -11,16 +11,11 @@ describe('Promise bucket', function() {
         scope = $rootScope;
     }));
 
-    it('should call the function and return', function() {
-        var f = function() { return 1; };
-        expect(pb.hit('f', f)).toBe(1);
-    });
-
-    it('should raise error if function is not valid', function() {
+    it('should raise error if promise is not valid', function() {
         expect(function() { pb.hit(); }).toThrow();
     });
 
-    it('should call the promise and resolve', function() {
+    it('should call the given promise and resolve', function() {
         var f = function() { return $q.when(1); };
         var result;
         pb.hit('f', f).then(function(res) {
@@ -28,6 +23,26 @@ describe('Promise bucket', function() {
         });
         scope.$apply();
         expect(result).toBe(1);
-    })
+    });
+
+    it('should call the given function and return', function() {
+        var f = function() { return 1; };
+        var result;
+        pb.hit('f', f).then(function(res) {
+            result = res;
+        });
+        scope.$apply();
+        expect(result).toBe(1);
+    });
+
+    it('should call the given promise and reject', function() {
+        var f = function() { return $q.when($q.reject(1)); };
+        var result;
+        pb.hit('f', f).then(function() {}, function(res) {
+            result = res;
+        });
+        scope.$apply();
+        expect(result).toBe(1);
+    });
 
 });
